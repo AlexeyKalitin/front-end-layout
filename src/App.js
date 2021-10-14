3beeffa6deb0f6b970e8e92023c42dbc944bfc3c
Alex <lksjkltn@gmail.com>
2021-10-14 12:43:41 +0300

    First prototype

--- src/App.js
+++ src/App.js
@@ -1,19 +1,38 @@
 import arrowup from "./arrowup.svg";
-import "./App.css";
-import trashbin from "./trash.svg";
-import icon from "./icon.svg";
 import arrowdown from "./arrowdown.svg";
 import "./style.css"
-import React from "react";
+import React, { useState } from 'react';
 import TodoList from "./components/TodoList";
 
 function App() {
+  const initialValue = ["2"]
+  const [list, setList] = useState(initialValue);
+  const [value, setValue] = useState("");
+
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  }
+
   return (
     <div className="conteiner">
       <div className="todo">
         <h1>TODO</h1>
         <div className="input">
-          <input placeholder="Type task" className="todo__text" type="text" />
+          <input
+            onKeyDown={(ev) => {
+              if (ev.keyCode == 13) {
+                setList([...list, value]);
+                setValue("")
+              }
+            }
+            }
+            value={value}
+            onChange={handleChange}
+            placeholder="Type task"
+            autoFocus
+            name="stateAttrName"
+            className="todo__text"
+            type="text" />
         </div>
       </div>
       <span className="filter">
@@ -33,10 +52,10 @@ function App() {
         </span>
       </span>
       <div className="todo-main">
-        <TodoList></TodoList>
+        <TodoList list={list} />
       </div>
       <span className="changer">
-        <button className="changer__button">&lt;&lt;</button>
+        <button className="changer__button">&gt;&gt;</button>
         <button className="changer__button">1</button>
         <button className="changer__button">2</button>
         <button className="changer__button">3</button>
