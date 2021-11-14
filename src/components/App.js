60ef55b347f10b2014a3990f5f4aced43618b7d6
AlexeyKalitin <92300161+AlexeyKalitin@users.noreply.github.com>
2021-11-14 16:21:34 +0300

    Add files via upload

--- src/components/App.js
+++ src/components/App.js
@@ -4,26 +4,23 @@ import TodosPage from '../TodosPage'
 import Login from './Login'
 import Register from './Register'
 import HomePage from './HomePage'
-import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles'
+
 import { CssBaseline } from '@material-ui/core'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
-const theme = createMuiTheme()
 
 export default function App() {
-
-
 	return (
-		<MuiThemeProvider theme={theme}>
+		<div>
 			<CssBaseline />
 			<Router>
 				<Switch>
-					<Route exact path="/" component={HomePage} />
-					<Route exact path="/login" component={Login} />
-					<Route exact path="/register" component={Register} />
-					<Route exact path="/todos" component={TodosPage} />
+					<Route exact path='/' component={HomePage} />
+					<Route exact path='/login' component={Login} />
+					<Route exact path='/register' component={Register} />
+					<Route exact path='/todos' component={TodosPage} />
 				</Switch>
 			</Router>
-		</MuiThemeProvider>
+		</div>
 	)
 }
